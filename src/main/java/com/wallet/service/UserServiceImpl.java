3e323b2431f6bb6a02169a235568139fc571c42e
Sunny <sunnysinghhrana@gmail.com>
2026-05-10 23:29:35 +0530

    - update UserServiceImpl and UserService to use dto

--- src/main/java/com/wallet/service/UserServiceImpl.java
+++ src/main/java/com/wallet/service/UserServiceImpl.java
@@ -1,18 +1,25 @@
 package com.wallet.service;
 
+import com.wallet.dto.RegisterRequestDto;
 import com.wallet.repository.UserRepository;
 import com.wallet.repository.WalletRepository;
 import com.wallet.model.User;
-import com.wallet.util.dbconnection.DBConnection;
+import com.wallet.util.DBConnection;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class UserServiceImplmnt implements UserService{
+public class UserServiceImpl implements UserService{
     private UserRepository userDao = new UserRepository();
     private WalletRepository walletDao = new WalletRepository();
 
-    public void register(User user) throws SQLException, ClassNotFoundException {
+    public void register(RegisterRequestDto dto) throws SQLException, ClassNotFoundException {
+
+        User user = new User();
+
+        user.setName(dto.getName());
+        user.setEmail(dto.getEmail());
+        user.setPassword(dto.getPassword());
         Connection conn = DBConnection.getConnection();
 
         try{

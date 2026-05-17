12b9d7497a11c6d3f06c5fcaa8b0c80f65879e7b
Sunny <sunnysinghhrana@gmail.com>
2026-05-17 22:43:54 +0530

    -Remodel WalletService

--- src/main/java/com/wallet/service/WalletService.java
+++ src/main/java/com/wallet/service/WalletService.java
@@ -3,11 +3,13 @@ package com.wallet.service;
 import com.wallet.model.Transaction;
 
 import java.math.BigDecimal;
-import java.sql.SQLException;
 import java.util.List;
 
 public interface WalletService {
-    public void transfer(long senderId, long receiverId, BigDecimal amount) throws SQLException, ClassNotFoundException;
-    public BigDecimal getBalance(long userId) throws SQLException, ClassNotFoundException;
-    public List<Transaction> getTransactionHistory(long userId) throws ClassNotFoundException, SQLException;
+
+    void transfer(long senderId, long receiverId, BigDecimal amount);
+
+    BigDecimal getBalance(long userId);
+
+    List<Transaction> getTransactionHistory(long userId);
 }

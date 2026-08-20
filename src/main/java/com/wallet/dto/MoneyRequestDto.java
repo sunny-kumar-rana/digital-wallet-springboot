a6ad490563e9c921fdd50393e74909ab81c2928a
Sunny <sunnysinghhrana@gmail.com>
2026-08-20 22:05:37 +0530

    -add MoneyRequest dto

--- src/main/java/com/wallet/dto/MoneyRequestDto.java
+++ src/main/java/com/wallet/dto/MoneyRequestDto.java
@@ -1,4 +1,31 @@
 package com.wallet.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
 public class MoneyRequestDto {
+
+    @NotNull(message = "Amount is required")
+    @DecimalMin(
+            value = "0.01",
+            message = "Amount must be greater than zero"
+    )
+    private BigDecimal amount;
+
+    public MoneyRequestDto() {
+    }
+
+    public MoneyRequestDto(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }

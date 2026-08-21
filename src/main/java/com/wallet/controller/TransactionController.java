f8ebb1b17ffd74b82629e235b8f7de1f47bd5dc2
Sunny <sunnysinghhrana@gmail.com>
2026-08-21 22:59:52 +0530

    -update TransactionController

--- src/main/java/com/wallet/controller/TransactionController.java
+++ src/main/java/com/wallet/controller/TransactionController.java
@@ -4,27 +4,25 @@ import com.wallet.model.Transaction;
 import com.wallet.service.WalletService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
 public class TransactionController {
 
-    private WalletService walletService;
+    private final WalletService walletService;
 
     @Autowired
-    public TransactionController(WalletService walletService){
+    public TransactionController(WalletService walletService) {
         this.walletService = walletService;
     }
 
     @GetMapping("/transactions")
-    public List<Transaction> getTransactions(@RequestParam long userId) {
+    public List<Transaction> getTransactions(
+            @RequestParam long userId
+    ) {
 
         return walletService.getTransactionHistory(userId);
     }

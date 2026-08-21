6078f86c0b23f27e87daa664ef78f2f3740b5a95
Sunny <sunnysinghhrana@gmail.com>
2026-08-21 23:00:06 +0530

    -update TransactionRepository

--- src/main/java/com/wallet/repository/TransactionRepository.java
+++ src/main/java/com/wallet/repository/TransactionRepository.java
@@ -1,16 +1,15 @@
 package com.wallet.repository;
 
 import com.wallet.model.Transaction;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+public interface TransactionRepository
+        extends JpaRepository<Transaction, Long> {
 
-    List<Transaction> findBySenderIdOrReceiverIdOrderByCreatedAtDesc(
-            long senderId,
-            long receiverId
-    );
+    List<Transaction> findBySenderIdOrReceiverIdOrderByCreatedAtDesc(long senderId, long receiverId);
 }

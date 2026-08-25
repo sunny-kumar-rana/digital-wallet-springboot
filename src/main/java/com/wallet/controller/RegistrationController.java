4617f2e194f07f03379c699963c99136276e2e67
Sunny <sunnysinghhrana@gmail.com>
2026-08-25 23:02:35 +0530

    -update RegistrationController

--- src/main/java/com/wallet/controller/RegistrationController.java
+++ src/main/java/com/wallet/controller/RegistrationController.java
@@ -1,18 +1,14 @@
 package com.wallet.controller;
 
+import com.wallet.dto.RegisterRequestDto;
 import com.wallet.service.UserService;
 
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.wallet.dto.RegisterRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
@@ -21,17 +17,22 @@ public class RegistrationController {
     private final UserService userService;
 
     @Autowired
-    public RegistrationController(UserService userService) {
+    public RegistrationController(
+            UserService userService
+    ) {
         this.userService = userService;
     }
 
     @PostMapping("/register")
-    public Map<String, String> register(@Valid @RequestBody RegisterRequestDto dto) {
+    public Map<String, String> register(
+            @Valid @RequestBody RegisterRequestDto dto
+    ) {
 
         userService.register(dto);
 
         return Map.of(
-                "message", "User Registered Successfully"
+                "message",
+                "User Registered Successfully"
         );
     }
 }

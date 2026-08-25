5e01dae6c776635e5fae858d1e25028f4d34574c
Sunny <sunnysinghhrana@gmail.com>
2026-08-25 23:02:48 +0530

    -update AuthController

--- src/main/java/com/wallet/controller/AuthController.java
+++ src/main/java/com/wallet/controller/AuthController.java
@@ -5,35 +5,41 @@ import com.wallet.model.User;
 import com.wallet.service.UserService;
 
 import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
 public class AuthController {
+
     private final UserService userService;
 
     @Autowired
-    public AuthController(UserService userService) {
+    public AuthController(
+            UserService userService
+    ) {
         this.userService = userService;
     }
 
     @PostMapping("/login")
-    public Map<String, Object> login(@Valid @RequestBody LoginRequestDto dto) {
+    public Map<String, Object> login(
+            @Valid @RequestBody LoginRequestDto dto
+    ) {
 
-        User user = userService.login(dto);
+        User user =
+                userService.login(dto);
+
+        String token =
+                userService.generateToken(user);
 
         return Map.of(
                 "message", "Login Successful",
+                "token", token,
                 "userId", user.getId(),
                 "name", user.getName()
         );
-
     }
 }
